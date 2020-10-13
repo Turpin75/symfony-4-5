6872bc3a8bd5e725fe27b4c4c3c202a1e8f9e97a
joseph <joseph.turpin.prestataire@sfr.com>
2020-10-13 17:40:15 +0200

    Create controller methods

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -8,12 +8,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class ToDoListController extends AbstractController
 {
     /**
-     * @Route("/todolist", name="to_do_list")
+     * @Route("/todolist", name="to-do-list")
      */
     public function index()
     {
         return $this->render('to_do_list/index.html.twig', [
             'controller_name' => 'ToDoListController',
         ]);
+    }
+
+    /**
+     * @Route("/create", name="create-task", methods={"POST"})
+     */
+    public function create()
+    {
+        exit('Create a new task !');
+    }
+
+    /**
+    * @Route("/switch-status/{id}", name="switch-status")
+    */
+    public function switchStatus($id)
+    {
+        exit("Switch status task $id!");
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete-task")
+     */
+    public function delete($id)
+    {
+        exit("Switch status task $id!");
     }
 }

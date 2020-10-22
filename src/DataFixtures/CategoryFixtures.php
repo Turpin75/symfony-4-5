2e1869a797e028aeca0154b894a0f36e77b5ca46
joseph <joseph.turpin.prestataire@sfr.com>
2020-10-22 21:17:39 +0200

    Make category fixtures part 1

--- src/DataFixtures/CategoryFixtures.php
+++ src/DataFixtures/CategoryFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +12,7 @@ class CategoryFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
       $this->loadMainCategories($manager);
+      $this->loadElectronics($manager);
     }
 
     private function loadMainCategories($manager)
@@ -25,8 +27,28 @@ class CategoryFixtures extends Fixture
         $manager->flush();
     }
 
+    private function loadElectronics($manager)
+    {
+        foreach($this->getElectronicsData() as $name)
+       {
+            $parent = $manager->getRepository(Category::class)->findOneBy(['name' => 'Electronics']);
+            $category = new Category();
+            $category->setName($name);
+            $category->setParent($parent);
+            $manager->persist($category);
+       }
+
+        $manager->flush();
+    }
+
     private function getMainCategoriesData()
     {
        return ['Electronics', 'Toys', 'Books', 'Movies']; 
     }
+
+    private function getElectronicsData()
+    {
+       return ['Cameras', 'Computers', 'Cell Phones'];
+    }
+
 }
